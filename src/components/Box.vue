40071bb801eba92404931cf650ea882b230201db
Akbar Hidayatuloh <01akbarh@gmail.com>
2024-05-17 07:19:15 +0700

    init

---
+++ src/components/Box.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-card :title="title">
+    <b-card-text>
+      {{ description }}
+    </b-card-text>
+    <a :href="link" rel="noreferrer noopenner" target="_blank"
+      ><b-button class="mb-2 mr-2" variant="outline-success"> Learn More </b-button></a
+    >
+    <router-link :to="demo"><b-button class="mb-2" variant="success"> See the demo </b-button></router-link>
+  </b-card>
+</template>
+
+<script lang="js">
+export default {
+  name: 'Home',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    link: {
+      type: String,
+      default: '/vue-google-adsense/'
+    },
+    demo: {
+      type: String,
+      default: '/'
+    },
+  }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hero">
+    <b-row mb="4" textAlign="center" align="center" justify="center" direction="column">
+      <h1>Vue Google Adsense</h1>
+      <h3>Vue.js Google Adsense Component with InFeed and InArticle Ads support</h3>
+    </b-row>
+
+    <b-row mb="2" align="center" justify="center" direction="column">
+      <a href="https://github.com/mazipan/vue-google-adsense">
+        <b-button variant="success">Github Repo</b-button>
+      </a>
+    </b-row>
+
+    <b-row align="center" justify="center">
+      <a href="https://www.npmjs.com/package/vue-google-adsense" target="_blank" rel="noopener noreferer" style="margin-right: 0.5rem">
+        <img alt="NPM Version" src="https://img.shields.io/npm/v/vue-google-adsense.svg" />
+      </a>
+      <a href="https://github.com/mazipan/vue-google-adsense" target="_blank" rel="noopener noreferer">
+        <img alt="GitHub Stars" src="https://img.shields.io/github/stars/mazipan/vue-google-adsense.svg?style=social" />
+      </a>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
